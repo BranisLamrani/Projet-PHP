b524568797ebe76ae5ed71df7522af1cd110b198
Naomi <npaulmin@yahoo.Com>
2017-01-10 14:14:43 +0100

    php pour la connexion des membres

---
+++ projetconnexion.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$dsn = 'mysql:dbname=projet_php;host=localhost';
+$user = 'root';
+$password = '';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage(); }
+
+if (!empty($_POST)) {
+    $login = $dbh->prepare('SELECT * FROM profil WHERE mail = :mail AND password = :password');
+    $login->execute([
+        ':mail' => $_POST['mail'],
+        ':password' => $_POST['password']
+    ]);
+}
+
+    $users = $login->fetchAll();
+
+    if(count($users) > 0) {
+        $_SESSION['connected'] = true;
+        $_SESSION['id'] = $users[0] ['id']; //enregistrement de l'id membre en session
+        header('location:account.php');
+    } else {
+        echo 'Connexion impossible. Veuillez réessayer.';
+    }
