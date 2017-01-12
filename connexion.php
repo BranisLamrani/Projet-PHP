c4f93a3162d11d64510453ecf63794739a3920bb
Naomi <npaulmin@yahoo.Com>
2017-01-12 10:53:21 +0100

    creation nouvelle page pour la connexion

--- connexion.php
+++ connexion.php
@@ -1,10 +1,21 @@
 <?php
+session_start();
+
+$dsn = 'mysql:dbname=projet_php;host=localhost';
+$user = 'root';
+$password = '';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
 
 if (!empty($_POST)) {
+    //$crypted_password = crypt('connexion_password', 'branisnaomipratheepa');
     $login = $dbh->prepare('SELECT * FROM profil WHERE mail = :mail AND password = :password');
     $login->execute([
-        ':mail' => $_POST['mail'],
-        ':password' => $_POST['password']
+        ':mail' => $_POST['connexion_mail'],
+        ':password' => $_POST['connexion_password']
     ]);
     $users = $login->fetchAll();
 
@@ -16,3 +27,13 @@ if (!empty($_POST)) {
         echo 'Connexion impossible. Veuillez réessayer.'; //la ligne s'affiche même lors de l'inscription
     }
 }
+
+?>
+
+<div class="connexion">
+    <form action="" method="post">
+        <input type="text" name="connexion_mail" id="connexion_mail" placeholder="Identifiant">
+        <input type="password" name="connexion_password" id="connexion_password" placeholder="Mot de passe">
+        <button type="submit">Connexion</button>
+    </form>
+</div>
