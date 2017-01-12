1be43516c781b1e1a29145e852bfdbc36ce228f5
Naomi <npaulmin@yahoo.Com>
2017-01-12 07:11:44 +0100

    mise à jour des fichiers index et membre

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/index.css">
+
+</head>
+<body>
+    <header>
+        <div class="titre">
+            Name
+        </div>
+        <div class="connexion">
+            <?php
+            require 'connexion.php';
+            ?>
+            <form action="" method="post">
+                <input type="text" name="mail" id="mail" placeholder="Identifiant">
+                <input type="text" name="password" id="password" placeholder="Mot de passe">
+                <button type="submit">Connexion</button>
+            </form>
+        </div>
+    </header>
+
+        
+        
+    <?php
+    require 'inscription.php';
+    ?>
+    <div class="inscription">
+        <form action="#" method="post" enctype="multipart/form-data">
+            <h2>INSCRIPTION</h2>
+            <br>
+
+            <div>
+                <label for="name">Nom</label>
+                <br>
+                <input type="text" name="name" id="name">
+            </div>
+
+            <br>
+            <div>
+                <label for="inscription_mail">Email</label>
+                <br>
+                <input type="Email" name="inscription_mail" id="inscription_mail">
+            </div>
+
+            <br>
+
+            <div>
+                <label for="inscription_password">Mot de passe</label>
+                <br>
+                <input type="password" name="inscription_password" id="inscription_password">
+            </div>
+
+
+            <br>
+
+            <button type="submit"> S'inscrire</button>
+        </form>
+    </div>
+
+     
+     
+     <div class="galerie">
+            <div class="photo"></div>
+            <div class="photo"></div>
+            <div class="photo"></div>
+            <div class="photo"></div>
+            <div class="photo"></div>
+        
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+    </html>
---
+++ membre.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+$dsn = 'mysql:dbname=projet_php;host=localhost';
+$user = 'root';
+$password = '';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Membre</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+
+<div class="head">
+       <div class="titre">NAME</div>
+       <div class="membre">
+           <a href="accueil_membre.php">Accueil</a>
+           <a href="">Mon Compte</a>
+           <a href="index.php">Se Déconnecter</a>
+       </div>
+   </div>
+
+   <div class="galerie">
+       <?php
+       $image = $dbh->prepare('SELECT name, description, image FROM image');
+       $image->execute();
+       $image->fetchAll();
+       ?>
+   </div>
+
+<div>
+    <h1>    PAGE EN TRAVAUX
+    </h1>
+</div>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
