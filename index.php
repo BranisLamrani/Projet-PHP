a5e8df8cde3c10b88a63bbdeaebc0f230cf127f4
koneswaranp <pratheepa.koneswaran@ynov.com>
2017-01-12 15:48:18 +0100

    navbar centré

--- index.php
+++ index.php
@@ -34,21 +34,22 @@ try {
         <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
 
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Inscription</a></li>
                     <li><a href="connexion.php">Se connecter</a></li>
                 </ul>
-                <form class="navbar-form navbar-left">
-                    <div class="form-group">
-                        <input type="text"  placeholder="Identifiant">
-                        <input type="text" placeholder="Mot de passe">
-                    </div>
-                    <button type="Submit" class="connecte" >Connexion</button>
-                </form>
+
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
 
